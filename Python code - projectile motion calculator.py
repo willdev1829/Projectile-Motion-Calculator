5a5d2e4e251cf16dec8d5baee070e19c604ecbdd
willdev1829 <willhamill95@gmail.com>
2026-06-21 17:42:00 +0100

    Add files via upload
    Coded my 2nd main section of code, which is the function for calculating the total time of motion.

--- Python code - projectile motion calculator.py
+++ Python code - projectile motion calculator.py
@@ -2,6 +2,61 @@ import math #for use in calculations
 Horizontalcounter = 0 #counts number of horizontal values provided
 Verticalcounter = 0 #counts number of vertical values provided
 
+Hashorizontaldisplacement = str(input("Do you have value for horizontal displacement? "))
+Hashorizontaldisplacement = Hashorizontaldisplacement.lower()
+if Hashorizontaldisplacement == 'yes':
+    Horizontalcounter = Horizontalcounter + 1
+    horizontaldisplacement = float(input("Enter value of horizontal displacement: "))
+    
+Hashorizontalinitialvelocity = str(input("Do you have value for horizontal initial velocity? "))
+Hashorizontalinitialvelocity = Hashorizontalinitialvelocity.lower()
+if Hashorizontalinitialvelocity == 'yes':
+    Horizontalcounter = Horizontalcounter + 1
+    horizontalinitialvelocity = float(input("Enter value of horizontal initial velocity: "))
+
+Hashorizontalfinalvelocity = str(input("Do you have value for horizontal final velocity? "))
+Hashorizontalfinalvelocity = Hashorizontalfinalvelocity.lower()
+if Hashorizontalfinalvelocity == 'yes':
+    Horizontalcounter = Horizontalcounter + 1
+    horizontalfinalvelocity = float(input("Enter value of horizontal final velocity: "))
+    
+Hashorizontalacceleration = str(input("Do you have value for horizontal acceleration? "))
+Hashorizontalacceleration = Hashorizontalacceleration.lower()
+if Hashorizontalacceleration == 'yes':
+    Horizontalcounter = Horizontalcounter + 1
+    horizontalacceleration = float(input("Enter value of horizontal acceleration: "))
+    
+Hastotaltime = str(input("Do you have value for total time? "))
+Hastotaltime = Hastotaltime.lower()
+if Hastotaltime == 'yes':
+    Horizontalcounter = Horizontalcounter + 1
+    Verticalcounter = Verticalcounter + 1
+    totaltime = float(input("Enter value of total time: "))
+    
+Hasverticaldisplacement = str(input("Do you have value for vertical displacement? "))
+Hasverticaldisplacement = Hasverticaldisplacement.lower()
+if Hasverticaldisplacement == 'yes':
+    Verticalcounter = Verticalcounter + 1
+    verticaldisplacement = float(input("Enter value of vertical displacement: "))
+    
+Hasverticalinitialvelocity = str(input("Do you have value for vertical initial velocity? "))
+Hasverticalinitialvelocity = Hasverticalinitialvelocity.lower()
+if Hasverticalinitialvelocity == 'yes':
+    Verticalcounter = Verticalcounter + 1
+    verticalinitialvelocity = float(input("Enter value of vertical initial velocity: "))
+
+Hasverticalfinalvelocity = str(input("Do you have value for vertical final velocity? "))
+Hasverticalfinalvelocity = Hasverticalfinalvelocity.lower()
+if Hasverticalfinalvelocity == 'yes':
+    Verticalcounter = Verticalcounter + 1
+    verticalfinalvelocity = float(input("Enter value of vertical final velocity: "))
+    
+Hasverticalacceleration = str(input("Do you have value for vertical acceleration? "))
+Hasverticalacceleration = Hasverticalacceleration.lower()
+if Hasverticalacceleration == 'yes':
+    Verticalcounter = Verticalcounter + 1
+    verticalacceleration = float(input("Enter value of vertical acceleration: "))
+
 def calculateinitialvelocity(): #define function to calculate initial velocity
     if Horizontalcounter < 3 and Verticalcounter < 3: #if less than 3 values of horizontal or vertical motion given, impossible to calculate initial velocity
         print("Not enough information provided")
@@ -96,63 +151,48 @@ def calculateinitialvelocity(): #define function to calculate initial velocity
     print(initialvelocity) #prints value of initial velocity to user.
     return initialvelocity #returns value of initial velocity to main code.
             
+def calculatetotaltime(): #define function to calculate total time taken for motion
+    global horizontalinitialvelocity #ensures all variables with command have same value both inside and outside of this function
+    global horizontalfinalvelocity
+    global horizontaldisplacement
+    global horizontalacceleration
+    global totaltime
+    global verticalinitialvelocity
+    global verticalfinalvelocity
+    global verticaldisplacement
+    global verticalacceleration
 
+    if Horizontalcounter < 3 and Verticalcounter < 3: #if less than 3 values of horizontal or vertical motion given, impossible to calculate total time taken
+        print("Not enough information provided")
+        exit()
+    if Horizontalcounter > 2:
+        if Hashorizontalinitialvelocity != 'yes': #uses suvat equation to calculate horizontal initial velocity, which is then used to calculate total time.
+            horizontalinitialvelocity = ((horizontalfinalvelocity ** 2) - (2 * horizontalacceleration * horizontaldisplacement)) ** 0.5
+            totaltime = (horizontalfinalvelocity - horizontalinitialvelocity) / horizontalacceleration
+        elif Hashorizontaldisplacement != 'yes': #no horizontal displacement value, so uses suvat equation which doesn't involve horizontal displacement.
+            totaltime = (horizontalfinalvelocity - horizontalinitialvelocity) / horizontalacceleration
+        elif Hashorizontalfinalvelocity != 'yes': #uses suvat equation to calculate final velocity, which is then used to calculate total time.
+            horizontalfinalvelocity = ((horizontalinitialvelocity ** 2) + (2 * horizontalacceleration * horizontaldisplacement)) ** 0.5
+            totaltime = (horizontalfinalvelocity - horizontalinitialvelocity) / horizontalacceleration
+        else: #must not have horizontal acceleration value, so uses suvat equation not involving horizontal acceleration.
+            totaltime = ((2 * horizontaldisplacement) / (horizontalinitialvelocity + horizontalfinalvelocity))
+    elif Verticalcounter > 2: #code uses same logic as above, but repeated for vertical motion.
+        if Hasverticalinitialvelocity != 'yes': #uses suvat equation to calculate vertical initial velocity, which is then used to calculate total time.
+            verticalinitialvelocity = ((verticalfinalvelocity ** 2) - (2 * verticalacceleration * verticaldisplacement)) ** 0.5
+            totaltime = (verticalfinalvelocity - verticalinitialvelocity) / verticalacceleration
+        elif Hasverticaldisplacement != 'yes': #no vertical displacement value, so uses suvat equation which doesn't involve vertical displacement.
+            totaltime = (verticalfinalvelocity - verticalinitialvelocity) / verticalacceleration
+        elif Hasverticalfinalvelocity != 'yes': #uses suvat equation to calculate final velocity, which is then used to calculate total time.
+            verticalfinalvelocity = ((verticalinitialvelocity ** 2) + (2 * verticalacceleration * verticaldisplacement))
+            totaltime = (verticalfinalvelocity - verticalinitialvelocity) / verticalacceleration
+        else: #must not have vertical acceleration value, so uses suvat equation not involving vertical acceleration.
+            totaltime = ((2 * verticaldisplacement) / (verticalinitialvelocity + verticalfinalvelocity))
+    else:
+        print("Not enough information provided")
+        exit()
+    print(totaltime) #prints value of total time taken to user.
+    return totaltime #returns value of total time taken to main code.
 
-
-Hashorizontaldisplacement = str(input("Do you have value for horizontal displacement? "))
-Hashorizontaldisplacement = Hashorizontaldisplacement.lower()
-if Hashorizontaldisplacement == 'yes':
-    Horizontalcounter = Horizontalcounter + 1
-    horizontaldisplacement = float(input("Enter value of horizontal displacement: "))
-    
-Hashorizontalinitialvelocity = str(input("Do you have value for horizontal initial velocity? "))
-Hashorizontalinitialvelocity = Hashorizontalinitialvelocity.lower()
-if Hashorizontalinitialvelocity == 'yes':
-    Horizontalcounter = Horizontalcounter + 1
-    horizontalinitialvelocity = float(input("Enter value of horizontal initial velocity: "))
-
-Hashorizontalfinalvelocity = str(input("Do you have value for horizontal final velocity? "))
-Hashorizontalfinalvelocity = Hashorizontalfinalvelocity.lower()
-if Hashorizontalfinalvelocity == 'yes':
-    Horizontalcounter = Horizontalcounter + 1
-    horizontalfinalvelocity = float(input("Enter value of horizontal final velocity: "))
-    
-Hashorizontalacceleration = str(input("Do you have value for horizontal acceleration? "))
-Hashorizontalacceleration = Hashorizontalacceleration.lower()
-if Hashorizontalacceleration == 'yes':
-    Horizontalcounter = Horizontalcounter + 1
-    horizontalacceleration = float(input("Enter value of horizontal acceleration: "))
-    
-Hastotaltime = str(input("Do you have value for total time? "))
-Hastotaltime = Hastotaltime.lower()
-if Hastotaltime == 'yes':
-    Horizontalcounter = Horizontalcounter + 1
-    Verticalcounter = Verticalcounter + 1
-    totaltime = float(input("Enter value of total time: "))
-    
-Hasverticaldisplacement = str(input("Do you have value for vertical displacement? "))
-Hasverticaldisplacement = Hasverticaldisplacement.lower()
-if Hasverticaldisplacement == 'yes':
-    Verticalcounter = Verticalcounter + 1
-    verticaldisplacement = float(input("Enter value of vertical displacement: "))
-    
-Hasverticalinitialvelocity = str(input("Do you have value for vertical initial velocity? "))
-Hasverticalinitialvelocity = Hasverticalinitialvelocity.lower()
-if Hasverticalinitialvelocity == 'yes':
-    Verticalcounter = Verticalcounter + 1
-    verticalinitialvelocity = float(input("Enter value of vertical initial velocity: "))
-
-Hasverticalfinalvelocity = str(input("Do you have value for vertical final velocity? "))
-Hasverticalfinalvelocity = Hasverticalfinalvelocity.lower()
-if Hasverticalfinalvelocity == 'yes':
-    Verticalcounter = Verticalcounter + 1
-    verticalfinalvelocity = float(input("Enter value of vertical final velocity: "))
-    
-Hasverticalacceleration = str(input("Do you have value for vertical acceleration? "))
-Hasverticalacceleration = Hasverticalacceleration.lower()
-if Hasverticalacceleration == 'yes':
-    Verticalcounter = Verticalcounter + 1
-    verticalacceleration = float(input("Enter value of vertical acceleration: "))
 
 #ask user what value they want to calculate
 
@@ -164,8 +204,8 @@ if Variabletocalculate == "initial velocity":
     initialvelocity = calculateinitialvelocity()
 #elif Variabletocalculate == "final velocity":
     
-#elif Variabletocalculate == "time taken":
-    
+elif Variabletocalculate == "time taken":
+    totaltime = calculatetotaltime()
 #if Variabletocalculate == "total distance travelled":
     
 #if Variabletocalculate == "tallest height reached":
